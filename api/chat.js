9f05a3f1c3223230c07296f04b6e0748fc5c057e
yerkebulanwork01-creator <yerkebulan.work01@gmail.com>
2026-03-29 18:23:55 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     if (req.method !== 'POST') {
       return res.status(405).json({ error: 'Method not allowed' });
@@ -57,4 +57,4 @@ export default async function handler(req, res) {
       details: error?.message || 'Белгісіз қате'
     });
   }
-}
+};
